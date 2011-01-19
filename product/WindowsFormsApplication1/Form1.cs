d1719f8081a58eaca4adc5368aac421a61ad8ecb
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-19 17:07:18 -0500

    Added the winform project

---
+++ product/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using nothinbutdotnetstore.presentation;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form,EditProfileView
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            new EditProfilePresenter(this).initialize();
+        }
+
+        public bool IsPostBack
+        {
+            get { return false; }
+        }
+
+        public void display(EditProfileDetail current_user_details)
+        {
+            ux_first_name.Text = current_user_details.FirstName;
+        }
+    }
+}
