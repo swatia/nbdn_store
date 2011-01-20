f42bce7af3ae5bc7417e967df633176744a63732
clarkrb <rclark@srobo.com>
2011-01-20 10:07:28 -0500

    added test

---
+++ product/nothinbutdotnetstore.specs/dataaccesslayer/DatabaseGatewaySpecs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using nothinbutdotnetstore.dataaccesslayer;
+using NUnit.Framework;
+
+namespace nothinbutdotnetstore.specs.dataaccesslayer
+{
+    class DatabaseGatewaySpecs
+    {
+        public class when_creating_a_connection_from_a_legitimate_connection_settings_item
+        {
+            [Test]
+            public void should_create_a_connection_with_the_right_details()
+            {
+                DataTable dt = DatabaseGateway.executequery("SELECT * FROM customers");
+
+                Assert.IsInstanceOf(typeof(DataTable), dt);
+            }
+        }
+    }
+}
---
+++ product/nothinbutdotnetstore/dataaccesslayer/DatabaseGateway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+namespace nothinbutdotnetstore.dataaccesslayer
+{
+    public static class DatabaseGateway
+    {
+        public static DataTable executequery(string s)
+        {
+            return null;
+        } 
+    }
+}
