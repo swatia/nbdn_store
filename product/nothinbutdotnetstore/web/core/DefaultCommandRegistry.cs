121ad2cae88f96effb1e5fa365567e930883336f
swatia <sagarwal@pressganey.com>
2011-01-21 16:24:58 -0500

    updated

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+        readonly IEnumerable<RequestCommand> all_available_commands;
+
         public DefaultCommandRegistry(IEnumerable<RequestCommand> all_available_commands)
         {
-            throw new NotImplementedException();
+            this.all_available_commands = all_available_commands;
         }
 
         public RequestCommand get_the_command_that_can_run(Request request)
         {
-            throw new NotImplementedException();
+            return all_available_commands.Where(x => x.can_process(request)).FirstOrDefault();
         }
     }
 }
