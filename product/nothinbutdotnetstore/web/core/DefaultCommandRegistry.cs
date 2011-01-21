8d1bc4fbbbfb922ef567d65b6820c47a9562f0b6
mpondo <mpondo@srobo.com>
2011-01-21 16:34:38 -0500

    msg

--- product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/core/DefaultCommandRegistry.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,7 +15,9 @@ namespace nothinbutdotnetstore.web.core
 
         public RequestCommand get_the_command_that_can_run(Request request)
         {
-            return all_available_commands.First(x => x.can_process(request));
+            RequestCommand command = all_available_commands.FirstOrDefault(x => x.can_process(request));
+            if (command == null) command = new MissingRequestCommand();
+            return command;
         }
     }
 }
