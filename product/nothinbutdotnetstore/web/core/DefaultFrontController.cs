030a5299d65be62d84f050ec0e093def71d1d50d
swatia <sagarwal@pressganey.com>
2011-01-21 16:12:52 -0500

    updated

--- product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/core/DefaultFrontController.cs
@@ -4,14 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
     public class DefaultFrontController : FrontController
     {
+        readonly CommandRegistry command_registry;
+
         public DefaultFrontController(CommandRegistry command_registry)
         {
-            throw new NotImplementedException();
+            this.command_registry = command_registry;
         }
 
         public void handle(Request request)
         {
-            throw new NotImplementedException();
+            command_registry.get_the_command_that_can_run(request).run( request);
         }
     }
 }
