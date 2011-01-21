90cb809d8e093b1ea85895886065d968683188cc
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-21 16:42:30 -0500

    Updated the RequestCommandSpecs

---
+++ product/nothinbutdotnetstore.specs/web/RequestCommandSpecs.cs
@@ -0,0 +1,17 @@
+using nothinbutdotnetstore.specs.utility;
+using NUnit.Framework;
+
+namespace nothinbutdotnetstore.specs.web
+{
+    public class RequestCommandSpecs
+    {
+        public class when_determining_if_it_can_process_a_request : BaseConcern
+        {
+            [Test]
+            public void should_make_the_determination_by_using_the_request_criteria()
+            {
+
+            } 
+        } 
+    }
+}
