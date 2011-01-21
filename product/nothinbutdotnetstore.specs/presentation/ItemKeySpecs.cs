f59009e7c38bf617f26c37fe74521aa8a582634c
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-01-21 14:09:56 -0500

    Updated the specs

--- product/nothinbutdotnetstore.specs/presentation/ItemKeySpecs.cs
+++ product/nothinbutdotnetstore.specs/presentation/ItemKeySpecs.cs
@@ -9,18 +9,32 @@ namespace nothinbutdotnetstore.specs.presentation
         {
             string result;
             string key_name;
+            ItemKey sut;
+
+            [SetUp]
+            protected void arrange()
+            {
+                key_name = "blah";
+                sut = new ItemKey(key_name);
+            }
 
             [Test]
             public void should_return_the_name_of_the_key()
             {
-                key_name = "blah";
-
-                var sut = new ItemKey(key_name);
-
                 result = sut;
 
                 Assert.AreEqual(key_name,result);
-            } 
+            }
+
+            [Test]
+            public void should_return_the_name_of_the_key_when_rendered_as_a_string()
+            {
+                result = sut.ToString();
+
+                Assert.AreEqual(key_name,sut.ToString());
+ 
+            }
+
         } 
     }
 }
